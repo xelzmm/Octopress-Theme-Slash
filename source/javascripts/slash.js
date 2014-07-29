79f16dbe6afdccd8d7066cca7350ae4442bd3b77
xelz <xelzmm@gmail.com>
2014-07-29 13:02:27 +0800

    fix jquery not found bug, remove fancybox, optimize tags div

--- source/javascripts/slash.js
+++ source/javascripts/slash.js
@@ -14,24 +14,7 @@
 		});
 	};
 
-	// Append caption after pictures
-	var appendCaption = function(){
-		$('.entry-content').each(function(i){
-			var _i = i;
-			$(this).find('img').each(function(){
-				var alt = this.alt;
-
-				if (alt != ''){
-					$(this).after('<span class="caption">'+alt+'</span>');
-				}
-
-				$(this).wrap('<a href="'+this.src+'" title="'+alt+'" class="fancybox" rel="gallery'+_i+'" />');
-			});
-		});
-	};
-
 	externalLinks(); // Delete or comment this line to disable opening external links in new window
-	appendCaption(); // Delete or comment this line to disable caption
 
 	var mobilenav = $('#mobile-nav');
 
